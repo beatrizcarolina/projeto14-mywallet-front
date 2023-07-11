3bd990964fc5af2cf5fa7a32f18b7a235c11baa7
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-07-11 15:06:52 -0300

    configure HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,121 +1,156 @@
-import styled from "styled-components"
-import { BiExit } from "react-icons/bi"
-import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai"
+import styled from "styled-components";
+import axios from "axios";
+import { BiExit } from "react-icons/bi";
+import { AiOutlineMinusCircle, AiOutlinePlusCircle } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import { UserContext } from "../UserContext";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-  return (
-    <HomeContainer>
-      <Header>
-        <h1>Olá, Fulano</h1>
-        <BiExit />
-      </Header>
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const [data, setData] = useState({ 
+        transactions: [],
+        balance: 0,
+    });
+    const [balance, setBalance] = useState("positivo");
 
-      <TransactionsContainer>
-        <ul>
-          <ListItemContainer>
-            <div>
-              <span>30/11</span>
-              <strong>Almoço mãe</strong>
-            </div>
-            <Value color={"negativo"}>120,00</Value>
-          </ListItemContainer>
+    function signOut() {
+        console.log(user.token);
+        axios
+            .delete(`${import.meta.env.VITE_API_URL}/signout`, {
+                headers: { Authorization: `Bearer ${user.token}` },
+            })
+            .then(() => {
+                localStorage.removeItem("user");
+                navigate("/");
+            })
+            .catch(() => {
+                localStorage.removeItem("user");
+                navigate("/");
+            });
+    }
 
-          <ListItemContainer>
-            <div>
-              <span>15/11</span>
-              <strong>Salário</strong>
-            </div>
-            <Value color={"positivo"}>3000,00</Value>
-          </ListItemContainer>
-        </ul>
+    function getTransaction() {
+        console.log(user.token);
+        axios
+            .get(`${import.meta.env.VITE_API_URL}/home`, {
+                headers: { Authorization: `Bearer ${user.token}` },
+            })
+            .then((response) => {
+                response.data.transactions.reverse();
+                setData(response.data);
+                if (response.data.balance>=0) {
+                    setBalance("positivo");
+                } else {
+                    setBalance("negativo");
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+                if (error.response) {
+                    return alert(`${error.response.data}. Error ${error.response.status}: ${error.response.statusText}`);
+                }
+                return alert(error.message);                
+            });
+    }
 
-        <article>
-          <strong>Saldo</strong>
-          <Value color={"positivo"}>2880,00</Value>
-        </article>
-      </TransactionsContainer>
+    useEffect(() =>  {
+        if (user && user.token) {
+            return getTransaction();
+        }
+        return navigate("/");
+    }, []);
 
 
-      <ButtonsContainer>
-        <button>
-          <AiOutlinePlusCircle />
-          <p>Nova <br /> entrada</p>
-        </button>
-        <button>
-          <AiOutlineMinusCircle />
-          <p>Nova <br />saída</p>
-        </button>
-      </ButtonsContainer>
+    return (
+        <HomeContainer>
+            <Header>
+                <h1 data-test="user-name">{`Olá, ${data.name}`}</h1>
+                <BiExit onClick={signOut} data-test="logout"/>
+            </Header>
 
-    </HomeContainer>
-  )
+            <TransactionsContainer>
+                <article>
+                    <strong>Saldo</strong>
+                    <Value color={balance} data-test="total-amount">{Math.abs(data.balance).toFixed(2).replace('.', ',')}</Value>
+                </article>
+            </TransactionsContainer>
+
+            <ButtonsContainer>
+                <button onClick={() => navigate("/nova-transacao/entrada")} data-test="new-income">
+                    <AiOutlinePlusCircle />
+                    <p>
+                        Nova <br /> entrada
+                    </p>
+                </button>
+                <button onClick={() => navigate("/nova-transacao/saida")} data-test="new-expense">
+                    <AiOutlineMinusCircle />
+                    <p>
+                        Nova <br />
+                        saída
+                    </p>
+                </button>
+            </ButtonsContainer>
+        </HomeContainer>
+    );
 }
 
 const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: calc(100vh - 50px);
-`
-const Header = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 2px 5px 2px;
-  margin-bottom: 15px;
-  font-size: 26px;
-  color: white;
-`
-const TransactionsContainer = styled.article`
-  flex-grow: 1;
-  background-color: #fff;
-  color: #000;
-  border-radius: 5px;
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  article {
     display: flex;
-    justify-content: space-between;   
-    strong {
-      font-weight: 700;
-      text-transform: uppercase;
-    }
-  }
-`
-const ButtonsContainer = styled.section`
-  margin-top: 15px;
-  margin-bottom: 0;
-  display: flex;
-  gap: 15px;
-  
-  button {
-    width: 50%;
-    height: 115px;
-    font-size: 22px;
-    text-align: left;
+    flex-direction: column;
+    height: calc(100vh - 50px);
+`;
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 2px 5px 2px;
+    margin-bottom: 15px;
+    font-size: 26px;
+    color: white;
+`;
+const TransactionsContainer = styled.article`
+    flex-grow: 1;
+    background-color: #fff;
+    color: #000;
+    border-radius: 5px;
+    padding: 16px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    p {
-      font-size: 18px;
+    overflow: scroll;
+    article {
+        display: flex;
+        justify-content: space-between;
+        strong {
+            font-weight: 700;
+            text-transform: uppercase;
+        }
     }
-  }
-`
+`;
+const ButtonsContainer = styled.section`
+    margin-top: 15px;
+    margin-bottom: 0;
+    display: flex;
+    gap: 15px;
+
+    button {
+        width: 50%;
+        height: 115px;
+        font-size: 22px;
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        p {
+            font-size: 18px;
+        }
+    }
+`;
 const Value = styled.div`
-  font-size: 16px;
-  text-align: right;
-  color: ${(props) => (props.color === "positivo" ? "green" : "red")};
-`
-const ListItemContainer = styled.li`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 8px;
-  color: #000000;
-  margin-right: 10px;
-  div span {
-    color: #c6c6c6;
-    margin-right: 10px;
-  }
-`
+    font-size: 16px;
+    text-align: right;
+    color: ${(props) => (props.color === "positivo" ? "#00ff00" : "#ff0000")};
+`;
